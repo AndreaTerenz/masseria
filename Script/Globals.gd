b7b753f0a564a46a8d6eda3c3cecd6b533248dcb
ALUnibo <alberto.luise@studio.unibo.it>
2024-09-29 01:07:46 +0200

    Non siamo solo dei numeri

--- Script/Globals.gd
+++ Script/Globals.gd
@@ -1,7 +1,20 @@
 extends Node
+class_name Global
+
+static var italian_names = [
+	"Alessandro", "Andrea", "Antonio", "Bruno", "Carlo", "Cristiano", "Domenico", "Enrico", "Fabio", "Federico", 
+	"Francesco", "Gabriele", "Gianluca", "Giorgio", "Giovanni", "Giuseppe", "Leonardo", "Lorenzo", "Luca", "Marco", 
+	"Mario", "Matteo", "Michele", "Nicola", "Paolo", "Pietro", "Roberto", "Salvatore", "Simone", "Stefano", 
+	"Tommaso", "Vincenzo", "Riccardo", "Giulio", "Davide", "Edoardo", "Alberto", "Claudio", "Daniele", "Emanuele",
+	"Alessandra", "Alice", "Anna", "Arianna", "Barbara", "Beatrice", "Bianca", "Camilla", "Carlotta", "Caterina", 
+	"Chiara", "Cristina", "Daniela", "Elena", "Elisabetta", "Emma", "Federica", "Francesca", "Gabriella", "Ginevra", 
+	"Giulia", "Ilaria", "Laura", "Leonora", "Livia", "Lucrezia", "Lucia", "Maria", "Martina", "Michela", "Monica", 
+	"Nicole", "Paola", "Sara", "Serena", "Silvia", "Sofia", "Stefania", "Valentina", "Vittoria", "Veronica",
+	"Andrea", "Gabriele", "Nicola", "Samuele", "Sasha", "Luca", "Elia", "Gianni"]
 
 func _ready() -> void:
-	seed(69420)
+	seed(42)
+	italian_names.shuffle()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("db_quit"):
